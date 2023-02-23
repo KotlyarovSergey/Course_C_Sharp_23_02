47a256a29bf65faa5469e76380b53b343e0a15d9
KotlyarovSergey <zews-sv@rambler.ru>
2023-02-23 16:11:16 +0300

    First exercise complete

---
+++ Lesson_1/HW/1_1_HW/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int numberA  = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int numberB  = int.Parse(Console.ReadLine()!);
+
+if (numberA > numberB)
+{
+    //Console.WriteLine("max = " + $"{numberA}");
+    Console.WriteLine($"max = {numberA}");
+}
+else if (numberB > numberA)
+{
+    Console.WriteLine($"max = {numberB}");
+}
+else    // сюда попадаем только если числа равны
+{
+    Console.WriteLine("Числа равны");
+}
+
+        
