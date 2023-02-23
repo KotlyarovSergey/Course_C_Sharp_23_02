2ac45aaea391fc3efc134fa9944c4f559bf7bac0
KotlyarovSergey <zews-sv@rambler.ru>
2023-02-23 11:45:39 +0300

    Task 1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int num = int.Parse(Console.ReadLine()!);
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Вторики");
+}
+else if (num == 3)
+{
+    Console.WriteLine("сруда");
+}
+else if (num == 4)
+{
+    Console.WriteLine("четверг");
+}
+else if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (num == 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (num == 7)
+{
+    Console.WriteLine("воскреснье");
+}
+else
+{
+    Console.WriteLine("ошибка ввода");
+}
+
