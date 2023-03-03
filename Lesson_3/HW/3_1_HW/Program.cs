77822cefd74b51750286450c21e6e2d5d9f476ba
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-03 12:29:16 +0300

    HW 3-1

---
+++ Lesson_3/HW/3_1_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает на вход пятизначное число
+// и проверяет, явлется ли оно палиндромом
+
+void Palindrom(int num)
+{
+    if (num > 99999 || num < 10000)
+        Console.WriteLine("Error");
+    else if (num % 10 == num / 10000 && num / 10 % 10 == num / 1000 % 10)
+        Console.WriteLine("Ура! Это палиндром");
+    else
+        Console.WriteLine("Увы, это просто число");
+}
+
+Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+Palindrom(num);
