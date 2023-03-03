f94c0516aab1688f3be7663f656b83db1a31a860
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-03 12:40:20 +0300

    HW 3-3

---
+++ Lesson_3/HW/3_3_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает на вход число N
+// и выдает таблицу кубов чисел от 1 до N
+
+void Powers (int n)
+{
+    int i = 1;
+    while(i <= n)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Powers(num);
