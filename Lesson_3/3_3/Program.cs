895d9c94275847a143a52af357896b0547a2aa3f
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-03 12:05:20 +0300

    Task 3-3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает на вход число N
+// и выдает таблицу квадратов чисел от 1 до N
+
+void Div (int n)
+{
+    int i =1;
+    while(i <= n)
+    {
+        Console.Write($"{Math.Pow(i, 2)} ");
+        i++;
+    }
+}
+
+Console.Write("Вводим: ");
+int num = int.Parse(Console.ReadLine()!);
+Div(num);
