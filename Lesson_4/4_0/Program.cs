50ed4c21b3c1ab8ca67dc812294394986c70dc09
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-07 10:43:52 +0300

    Task 4-0

---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая принимает
+// на выход число (А) и выдает сумму чисел от 1 до А
+
+int SumNum(int num)
+{
+    int allSum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        allSum += i;
+    }
+    return allSum;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int res = SumNum(A);
+Console.WriteLine(res);
