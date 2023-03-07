7073f6b47887ee83b0c377f338223a726513bafe
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-07 14:19:02 +0300

    HW 4-2

---
+++ Lesson_4/HW/4_2_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе
+
+int DigitSumm(int number)
+{
+    int sum = 0;
+    for(int i = 0; number > 0; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+
+Console.Write("Введите число ");
+Console.WriteLine(DigitSumm(int.Parse(Console.ReadLine()!)));
