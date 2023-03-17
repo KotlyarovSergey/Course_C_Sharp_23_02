da472bd29d72583b07c7e1c37913a84eeeffdc47
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-17 12:42:49 +0300

    all Class Work

---
+++ Lesson_7/7_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// 1. Задайте двумерный массив размера m на n,
+	//    каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+	//    Выведите полученный массив на экран.
+    
+    void Print(int[,] arr)
+	{
+	    int row = arr.GetLength(0);
+	    int column = arr.GetLength(1);
+	
+	    for (int i = 0; i < row; i++)
+	    {
+	        for (int j = 0; j < column; j++)
+	            Console.Write($" {arr[i, j]} ");
+	        Console.WriteLine();
+	    }
+	}
+	
+	int[,] MassNums(int row, int column)
+	{
+	    int[,] arr = new int[row, column];
+	
+	    for (int i = 0; i < row; i++)
+	        for (int j = 0; j < column; j++)
+	            arr[i, j] = i + j;
+	    return arr;
+	}
+	
+	int m = int.Parse(Console.ReadLine()!);
+	int n = int.Parse(Console.ReadLine()!);
+	
+	int[,] mass = MassNums(m, n);
+	Print(mass);
---
+++ Lesson_7/7_4/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Введите элемент, и найдите
+// первое его вхождение, выведите позиции по горизонтали и
+// вертикали, или напишите, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+string FindElement(int[,] array, int findedElement)
+{
+    string result = "Don't finded";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == findedElement)
+                return $"[{i + 1}, {j + 1}]";
+        }
+    }
+    return result;
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
+int finded = int.Parse(Console.ReadLine()!);
+Console.WriteLine(FindElement(mass, finded));
