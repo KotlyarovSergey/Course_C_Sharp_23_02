78228c09870a5460ea48db73713a62a9cc0eaf8d
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-20 17:28:08 +0300

    add Class Work

--- Lesson_8/8_2/Program.cs
+++ Lesson_8/8_2/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int GetUserDataInteger(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Print(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($"{array[i, j], -3} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] RandomArray(int rows, int columns, int minItem, int maxItem)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minItem, maxItem + 1);
+    return array;
+}
+
+int[,] TranspositionMatrix(int[,] array)
+{
+    int originalRows = array.GetLength(0);
+    int originalColmns = array.GetLength(1);
+    int[,] newMatrix = new int[originalColmns, originalRows];
+    for (int i = 0; i < originalRows; i++)
+    {
+        for (int j = 0; j < originalColmns; j++)
+        {
+            newMatrix[j, i] = array[i, j];
+        }
+    }
+    return newMatrix;
+}
+
+
+void TransQuadMatrix(int[,] array)
+{
+    for (int i = 1; i < array.GetLength(0) - 1; i++)
+    {
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+        }
+    }
+}
+
+void TransQuadMatrix2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+        }
+    }
+}
+
+
+
+int rows = GetUserDataInteger("Количество строк: ");
+int columns = GetUserDataInteger("Количество столбцов: ");
+int start = GetUserDataInteger("Минимальный элемент: ");
+int stop = GetUserDataInteger("Максимальный элемент: ");
+
+int[,] array = RandomArray(rows, columns, start, stop);
+Print(array);
+Console.WriteLine();
+
+//int[,] transArray = TranspositionMatrix(array);
+//Print(transArray);
+//Console.WriteLine();
+
+if (array.GetLength(0) == array.GetLength(1))
+{
+    TransQuadMatrix2(array);
+    Print(array);
+}
--- Lesson_8/8_3/Program.cs
+++ Lesson_8/8_3/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // 3. Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз
+// встречается элемент входных данных. Значения элементов массива 0..9
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine())!;
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(maxValue + 1);
+}
+
+string Print2DArray(int[,] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            res += array[i, j];
+            if (j != array.GetLength(1) - 1)
+                res += "\t";
+            else
+                res += "\n";
+        }
+    return res;
+}
+
+int[] Frequency(int[,] array, int maxValue)
+{
+    int[] dict = new int[maxValue + 1];
+
+    foreach (int element in array)
+    {
+        dict[element] += 1;
+    }
+    return dict;
+}
+
+string PrintFrequency(int[] array)
+{
+    string res = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            res += $"Число {i} встречается {array[i]} раз\n";
+    }
+    return res;
+}
+
+int numRows = InputNum("Строк: ");
+int numCol = InputNum("Столбцов: ");
+int[,] myArray = Create2DArray(numRows, numCol);
+int max = InputNum("Макс число: ");
+Fill2DArray(myArray, max);
+Console.WriteLine(Print2DArray(myArray));
+
+int[] dictonary = Frequency(myArray, max);
+Console.WriteLine(PrintFrequency(dictonary));
