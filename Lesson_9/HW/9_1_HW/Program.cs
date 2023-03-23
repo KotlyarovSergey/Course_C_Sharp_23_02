46cd613bb74ac66356fa4c9f6a2218fd06174568
KotlyarovSergey <zews-sv@rambler.ru>
2023-03-23 12:07:49 +0300

    Home Work

---
+++ Lesson_9/HW/9_1_HW/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int GetUserDataInteger(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void EvenNummsFromTo(int m, int n)
+{
+    if (m == n + 1) return;
+    EvenNummsFromTo(m, n - 1);
+    if (n % 2 == 0) System.Console.Write($"{n} ");
+}
+
+void EvenNummsFromToVer2(int m, int n)
+{
+    if (m >= n + 1) return;
+    EvenNummsFromTo(m, n - 2);
+    System.Console.Write($"{n} ");
+}
+
+
+
+
+int m = GetUserDataInteger("От: ");
+int n = GetUserDataInteger("До: ");
+
+EvenNummsFromTo(m, n);
+Console.WriteLine();
+
+if (n % 2 == 1) n--;
+EvenNummsFromToVer2(m, n);
---
+++ Lesson_9/HW/9_2_HW/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+
+int GetUserDataInteger(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SummNummsFromTo(int m, int n)
+{
+    if (m == n + 1) return 0;
+    return n + SummNummsFromTo(m, n - 1);
+}
+
+int SummNummsFromToVer2(int nFrom, int nTo)
+{
+    if(nFrom > nTo) return 0;
+    return nFrom + SummNummsFromToVer2(nFrom + 1, nTo);
+}
+
+
+
+
+int m = GetUserDataInteger("От: ");
+int n = GetUserDataInteger("До: ");
+
+Console.WriteLine(SummNummsFromTo(m, n));
+Console.WriteLine(SummNummsFromToVer2(m, n));
